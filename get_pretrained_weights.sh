1f6cfffcd3f4aadf1a69efc29a685c70634608ed
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2019-07-16 21:03:37 +0530

    pipeline update and ulmfit updates

---
+++ get_pretrained_weights.sh
@@ -0,0 +1,7 @@
+cd experiments/
+mkdir pretrained_models
+cd pretrained_models
+wget -c https://s3.amazonaws.com/fast-ai-modelzoo/wt103-fwd.tgz
+tar -zxvf wt103-fwd.tgz
+wget -c https://s3.amazonaws.com/fast-ai-modelzoo/wt103-bwd.tgz
+tar -zxvf wt103-bwd.tgz
